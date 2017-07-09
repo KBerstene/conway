db00021c1c0c55e89fc59d9719366c7418961940
Kevin T. Berstene <kberstene@gmail.com>
2017-07-08 22:38:25 -0400

    Fixed reset button failing to create new cell neighbor lists

--- grid.py
+++ grid.py
@@ -23,11 +23,43 @@ class Grid():
 			for j in range(self.gridHeight):
 				self.cells[i][j] = Cell(Position((i*20) + self.rect.left, (j*20) + self.rect.top))
 
+		# Create each cell's neighbors list
+		self.createCellNeighbors()
+		
 	def draw(self, surface):
 		# Iterate through grid and print white square as dead and black square as alive
 		for row in self.cells:
 			for cell in row:
 				pygame.draw.rect(surface, pygame.Color("black"), cell, not(cell.alive))
+	
+	def createCellNeighbors(self):
+		# Create neighbors list for each cell
+		for i in range(self.gridWidth):
+			for j in range(self.gridHeight):
+			# Clear neighbors list
+				self.cells[i][j].neighbors = []
+				
+			# Top left
+				if i > 0 and j > 0: self.cells[i][j].neighbors.append(self.cells[i-1][j-1])
+			# Top middle
+				if j > 0: self.cells[i][j].neighbors.append(self.cells[i][j-1])
+			# Top right
+				if j > 0 and i + 1 < self.gridWidth: self.cells[i][j].neighbors.append(self.cells[i+1][j-1])
+				
+			# Left
+				if i > 0: self.cells[i][j].neighbors.append(self.cells[i-1][j])
+			# Right
+				try: self.cells[i][j].neighbors.append(self.cells[i+1][j])
+				except: pass
+				
+			# Bottom left
+				if i > 0 and j + 1 < self.gridHeight: self.cells[i][j].neighbors.append(self.cells[i-1][j+1])
+			# Bottom middle
+				try: self.cells[i][j].neighbors.append(self.cells[i][j+1])
+				except: pass
+			# Bottom right
+				try: self.cells[i][j].neighbors.append(self.cells[i+1][j+1])
+				except: pass
 	
 	def collidepoint(self, pos):
 		if self.rect.collidepoint(pos):
@@ -59,40 +91,9 @@ class Grid():
 					newArray[i][j] = Cell(Position((i*20) + self.rect.left, (j*20) + self.rect.top))
 		
 		self.cells = newArray
-		
-		#create neighbors list
-		for i in range(self.gridWidth):
-			for j in range(self.gridHeight):
-				if i>0:
-					self.cells[i][j].neighbors.append(self.cells[i-1][j])
-					self.cells[i][j].neighbors.append(self.cells[i-1][j-1])
-					try:
-						self.cells[i][j].neighbors.append(self.cells[i-1][j+1])
-					except:
-						pass
-				if j>0:
-					self.cells[i][j].neighbors.append(self.cells[i][j-1])
-					try:
-						self.cells[i][j].neighbors.append(self.cells[i+1][j-1])
-					except:
-						pass
-				try:
-					self.cells[i][j].neighbors.append(self.cells[i+1][j+1])
-				except:
-					pass
-				try:
-					self.cells[i][j].neighbors.append(self.cells[i][j+1])
-				except:
-					pass
-				try:
-					self.cells[i][j].neighbors.append(self.cells[i+1][j])
-				except:
-					pass
-					
-				
-				
-
-		
+		self.createCellNeighbors()
+	
+	
 # Subclass of Rect to add alive/dead status
 class Cell(pygame.Rect):
 	# Constructor
